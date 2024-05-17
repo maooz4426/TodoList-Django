21160c0ea3417f69e67c66cb6d8c0fe719564709
MAOZ <maooz4426@gmail.com>
2024-05-17 22:57:53 +0900

    コミット残しをコミット

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from .models import Task
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+class ListView(generic.ListView):
+    model = Task
+    template_name = "task_list.html"
