51ca2559b7062a9343efe16fb8a84c7b65211b6f
MAOZ <maooz4426@gmail.com>
2024-05-23 06:02:30 +0900

    少しボタンのレイアウト調整

--- todolist/accounts/urls.py
+++ todolist/accounts/urls.py
@@ -3,7 +3,7 @@ from .views import SignupView,CustomLoginView,CustomLogoutView
 
 app_name = 'accounts'
 urlpatterns = [
-    path('signup/', SignupView.as_view(),name='sighup'),
+    path('signup/', SignupView.as_view(),name='signup'),
     path('login/',CustomLoginView.as_view(),name='login'),
     path('logout/',CustomLogoutView.as_view(),name='logout'),
 ]
